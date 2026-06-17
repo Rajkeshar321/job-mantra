c8f0c44162a0a0660e4350ab5b16ccd85d0acbd2
Rajkeser <rajkeshar.2024ug2073@iiitranchi.ac.in>
2026-06-17 15:53:15 +0530

    fixed case sensitivity issue again

--- backend/routes/tests.js
+++ backend/routes/tests.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import Test from '../models/test.js';
-import User from '../models/user.js';
+import user from '../models/user.js';
 import auth from '../middleware/auth.js';
 
 const router = express.Router();
